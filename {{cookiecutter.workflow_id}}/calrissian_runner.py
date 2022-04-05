a0b98f12a22b56147c813b22136367fd041e32ad
bbrauzzi <blasco.brauzzi@terradue.com>
2022-04-05 10:23:52 +0200

    calrissian runner update

--- {{cookiecutter.workflow_id}}/calrissian_runner.py
+++ {{cookiecutter.workflow_id}}/calrissian_runner.py
@@ -1,28 +1,27 @@
 import sys
+import zoo
 
 class CalrissianRunner(object):
-    
+
     def __init__(self, conf, inputs, outputs):
 
         self.conf = conf
         self.inputs = inputs
         self.outputs = outputs
-    
-    def update_status(self, progress):
 
-        self.zoo.update_status(self.conf, progress)
-        
     def execute(self):
 
         print("Job started",file=sys.stderr)
-
-        self.update_status(20)
+        self.conf["lenv"]["message"]="Started";
+        zoo.update_status(self.conf, 20)
 
         print("Job is running",file=sys.stderr)
-
-        self.update_status(100)
+        self.conf["lenv"]["message"]="Running";
+        zoo.update_status(self.conf, 50)
 
         print("Job has finished running",file=sys.stderr)
+        self.conf["lenv"]["message"]="Completed";
+        zoo.update_status(self.conf, 100)
 
         result_key = list(self.outputs.keys())[0]
         self.outputs[result_key]["value"] = ("Success!")
