21da8335939cc706a24612ad534d0755b52b92fa
bbrauzzi <blasco.brauzzi@terradue.com>
2023-06-21 11:43:57 +0200

    Update service.py

--- {{cookiecutter.service_name}}/service.py
+++ {{cookiecutter.service_name}}/service.py
@@ -116,7 +116,11 @@ def {{cookiecutter.workflow_id |replace("-", "_")  }}(conf, inputs, outputs):
     exit_status = runner.execute()
 
     if exit_status == zoo.SERVICE_SUCCEEDED:
-        outputs = runner.outputs.outputs
+        # TODO remove hardcoded key StacCatalogUri which is defined in the main.cwl
+        # Remove the "stac" output from runner.outputs.outputs["stac"]["value"] in previous phases
+        out = {"StacCatalogUri": runner.outputs.outputs["stac"]["value"]["StacCatalogUri"] }
+        json_out_string= json.dumps(out, indent=4)
+        outputs["stac"]["value"]=json_out_string
         return zoo.SERVICE_SUCCEEDED
 
     else:
