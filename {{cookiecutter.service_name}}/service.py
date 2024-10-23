a10d21c77a495026bde1cded047d19f3b4896319
Bernard Valentin <bernard.valentin@gmail.com>
2024-10-23 04:15:51 +0200

    Call the model service infer endpoint and return the response

--- {{cookiecutter.service_name}}/service.py
+++ {{cookiecutter.service_name}}/service.py
@@ -30,29 +30,39 @@ logger.remove()
 logger.add(sys.stderr, level="DEBUG")
 
 
-def request(method, baseurl, path, headers={}, data={}):
-    logger.info(f"Requesting: http://{baseurl}{path}")
+def request(method, baseurl, path, headers={}, data=None):
+    logger.info(f"HTTP Request: {method} http://{baseurl}{path}")
     conn = http.client.HTTPConnection(baseurl)
-    conn.request(method, path)
+    if data:
+        if isinstance(data, dict):
+            conn.request(method, path, json.dumps(data), headers)
+        else:
+            conn.request(method, path, data, headers)
+    else:
+        conn.request(method, path, data, headers)
     return conn.getresponse()
 
 
 def check_model(baseurl, basepath):
     res = request("GET", baseurl, basepath + "/ready")
-    logger.info(f"Model ready status: {res.status} {res.reason}")
-    logger.info("Model ready text: " + str(res.read()))
+    logger.info(f"Model Ready response status: {res.status} {res.reason}")
+    logger.info("Model Ready response text: " + str(res.read()))
     res = request("GET", baseurl, basepath)
-    logger.info(f"Model metadata status: {res.status} {res.reason}")
+    logger.info(f"Model Metadata response status: {res.status} {res.reason}")
     try:
         res_text = res.read().decode("utf-8")
-        logger.info("Model metadata:\n" + json.dumps(json.loads(res_text), indent=2))
+        logger.info("Model Metadata:\n" + json.dumps(json.loads(res_text), indent=2))
     except:
-        logger.info("Model metadata text: " + str(res.read()))
+        stack = traceback.format_exc()
+        logger.error(stack)
+        logger.info("Model Metadata response text: " + str(res.read()))
 
 
-def execute(inputs={}):
+def execute(baseurl, basepath, inputs={}):
     logger.info("Executing the model with inputs:\n" + json.dumps(inputs, indent=2))
-    # TODO INVOKE THE MODEL HERE
+    res = request("POST", baseurl, basepath + "/infer", data=inputs)
+    logger.info(f"Model Infer response status: {res.status} {res.reason}")
+    return res
 
 
 def fix_inputs(inputs):
@@ -90,24 +100,30 @@ def {{cookiecutter.workflow_id |replace("-", "_") }}(conf, inputs, outputs): # n
         model_basepath = "/v2/models/classifier"
 
         check_model(model_baseurl, model_basepath)
-        check_model("{{cookiecutter.service_name}}")
-        exec_outputs = execute(inputs=inputs)
-        logger.info("Execution outputs:\n" + str(exec_outputs))
+        infer_inputs = json.loads(inputs["model_inputs"]["value"])
+        res = execute(model_baseurl, model_basepath, inputs=infer_inputs)
+        try:
+            infer_bytes = res.read()
+            infer_outputs = json.loads(infer_bytes.decode("utf-8"))
+            logger.info("Model Infer outputs:\n" + json.dumps(infer_outputs, indent=2))
+        except:
+            stack = traceback.format_exc()
+            logger.error(stack)
+            logger.info("Model Infer outputs text: " + str(infer_bytes))
 
-        exit_status = zoo.SERVICE_SUCCEEDED
+        exit_status = zoo.SERVICE_SUCCEEDED if res.status == 200 else zoo.SERVICE_FAILED
 
         if exit_status == zoo.SERVICE_SUCCEEDED:
             logger.info(f"Storing the model output on disk")
-            model_outputs = { "model_outputs": [ 5.576883936610762 ] }
             data = {
                 "outputs": [{
-                    "data": model_outputs
+                    "data": infer_outputs
                 }]
             }
             logger.info(f"Setting model output into output key {list(outputs.keys())[0]}")
-            outputs[list(outputs.keys())[0]]["value"] = json.dumps(model_outputs)
+            outputs[list(outputs.keys())[0]]["value"] = json.dumps(infer_outputs)
             with open('output.json', 'w') as f:
-                json.dump(model_outputs, f, indent=2)
+                json.dump(infer_outputs, f, indent=2)
             logger.info("Outputs:\n" + json.dumps(outputs, indent=2))
             return zoo.SERVICE_SUCCEEDED
 
